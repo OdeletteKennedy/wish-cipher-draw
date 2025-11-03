b455df4b91379030e29b4b2b7f43a3f491d881d3
OdeletteKennedy <belkecliff41@outlook.com>
2025-11-02 16:00:00 -0800

    test: add Sepolia testnet deployment and test scripts

---
+++ test/FHELotterySepolia.ts
@@ -0,0 +1,13 @@
+﻿import { expect } from "chai";
+import { ethers } from "hardhat";
+
+describe("FHELottery Sepolia", function () {
+  it("Should deploy on Sepolia testnet", async function () {
+    const FHELottery = await ethers.getContractFactory("FHELottery");
+    const lottery = await FHELottery.deploy();
+    await lottery.waitForDeployment();
+    
+    const address = await lottery.getAddress();
+    expect(address).to.be.properAddress;
+  });
+});
