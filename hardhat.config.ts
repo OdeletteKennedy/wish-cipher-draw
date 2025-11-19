dd6299b46e6df77e081e187d0e91228bc6574aa4
OdeletteKennedy <belkecliff41@outlook.com>
2025-11-19 14:43:00 -0800

    chore: clean up comments in solidity compiler config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -73,12 +73,8 @@ const config: HardhatUserConfig = {
     version: "0.8.24",
     settings: {
       metadata: {
-        // Not including the metadata hash
-        // https://github.com/paulrberg/hardhat-template/issues/31
         bytecodeHash: "none",
       },
-      // Disable the optimizer when debugging
-      // https://hardhat.org/hardhat-network/#solidity-optimizer-support
       optimizer: {
         enabled: true,
         runs: 800,
